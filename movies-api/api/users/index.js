1b3c283569b49c4bf12ac7150994e247b56c2fc3
ViktoriaEmily <20085118@mail.setu.ie>
2022-11-29 16:34:55 +0000

    Topic 10 - Session BASED TRUTH

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import User from './userModel';
+import asyncHandler from "express-async-handler";
 
 const router = express.Router(); // eslint-disable-line
 
@@ -9,8 +10,17 @@ router.get('/', async (req, res) => {
     res.status(200).json(users);
 });
 
-// register(Create)/Authenticate User
-router.post('/', async (req, res) => {
+router.get('/:id/favourites', async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+        res.status(200).json(user.favourites);
+    } else {
+        res.status(404).json({ code: 404, msg: 'Unable to find favourites' });
+    }
+});
+
+// register
+router.post('/', asyncHandler(async (req, res) => {
     if (req.query.action === 'register') {  //if action is 'register' then save to DB
         await User(req.body).save();
         res.status(201).json({
@@ -18,12 +28,31 @@ router.post('/', async (req, res) => {
             msg: 'Successful created new user.',
         });
     }
-    else {  //Must be an authenticate then!!! Query the DB and check if there's a match
-        const user = await User.findOne(req.body);
-        if (!user) {
-            return res.status(401).json({ code: 401, msg: 'Authentication failed' });
-        }else{
-            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
+    else {  //NEW CODE!!!
+        const user = await User.findByUserName(req.body.username);
+        if (user.comparePassword(req.body.password)) {
+            req.session.user = req.body.username;
+            req.session.authenticated = true;
+            res.status(200).json({
+                success: true,
+                token: "temporary-token"
+              });
+        } else {
+            res.status(401).json('authentication failed');
+        }
+    }
+}));
+
+router.post('/:id/favourites', async (req, res) => {
+    const newFavourite = req.body;
+    if (newFavourite && newFavourite.id) {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            user.favourites.push(newFavourite);
+            user.save();
+            res.status(201).json({ code: 201, msg: "Added Favourite" });
+        } else {
+            res.status(404).json({ code: 404, msg: 'Unable to add favourites' });
         }
     }
 });
--- movies-api/index.js
+++ movies-api/index.js
@@ -5,9 +5,20 @@ import './db';
 import './seedData';
 import usersRouter from './api/users';
 import genresRouter from './api/genres';
+import session from 'express-session';
+import authenticate from './authenticate';
 
 
 dotenv.config();
+
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+};
 
 const app = express();
 
@@ -22,6 +33,18 @@ app.use('/api/users', usersRouter);
 
 app.use('/api/genres', genresRouter);
 
+app.use(errHandler);
+
+//session middleware
+app.use(session({
+  secret: 'ilikecake',
+  resave: true,
+  saveUninitialized: true
+}));
+
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
+
+//update /api/Movie route
+app.use('/api/movies', authenticate, moviesRouter);
